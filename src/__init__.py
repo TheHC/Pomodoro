dc71374d9191cd8dba366b4c68ea975e6aed4018
H-CHETTOUR <57678914+H-CHETTOUR@users.noreply.github.com>
2025-11-30 15:51:39 +0100

    Add Icon

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,29 @@
 # Only needed for access to command line arguments
 import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout,QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout,QPushButton, QMainWindow, QLineEdit,QTextEdit
 
 
 #sublass QMainWindows to customize app main windows
-class MainWindow(QMainWindow):
+class MMyApp(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("Pomodoro")
+        
+        self.inputtest=
         button = QPushButton("start")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
+        button.clicked.connect(self.the_button_was_toggled)
+        layout = QVBoxLayout()
+        layout.addWidget(button)
 
         #Set the central widget of the windows
         self.setCentralWidget(button)
-
+    def the_button_was_clicked(self):
+        print("clicked")
+    def the_button_was_toggled(self, checked):
+        print("checked?",checked)
 
 # Pass in sys.argv to allow command line arguments
 app = QApplication(sys.argv)
@@ -25,5 +35,6 @@ window.show()
 # Start the event loop.
 app.exec()
 
+#sys.exit(app.exec_()) to ensure the process is terminated upon closing
 # Your application won't reach here until you exit and the event
 # loop has stopped.
