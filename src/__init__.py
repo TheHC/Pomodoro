7759409f099d25c4b5ef8a61841645637ac9ddf7
H-CHETTOUR <57678914+H-CHETTOUR@users.noreply.github.com>
2025-12-02 03:37:54 +0100

    Add threading and Focus, Short states

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,53 @@ import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout,QPushButton, QMainWindow, QLineEdit,QTextEdit
 from PyQt6.QtGui import QIcon
 from PyQt6 import uic
+from PyQt6.QtCore import QRunnable, QThreadPool, QTimer, pyqtSlot
+from PySide6.QtCore import  Signal, QObject
+import time
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logger.info('Starting program')
+
+variable=0
+state_select = "Focus"
+class Worker(QRunnable):
+    """Worker thread"""
+    def __init__(self, time):
+        super().__init__()
+        self.time = time
+        self.signals = WorkerSignals()
+
+
+    @pyqtSlot()
+    def run(self):
+        # print("Thread start")
+        logger.info('Starting worker thread')
+        time.sleep(self.time)
+        # print(self.time)
+        # print("Thread end")
+        logger.info('Ended worker thread')
+        self.signals.finished.emit()
+
+
+class WorkerSignals(QObject):
+    finished=Signal()
+    progress=Signal(int)
+
+
 
 #sublass QMainWindows to customize app main windows
 class MyApp(QWidget):
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         super().__init__()
         uic.loadUi('gui.ui', self)
+        self.step="Focus"
+        self.button.clicked.connect(self.state_machine)
 
+        self.threadpool=QThreadPool()
+
+        self.state=1
+        self.focus_label.setStyleSheet("background-color: green")
         # self.setWindowTitle("Pomodoro")
         # self.setWindowIcon(QIcon('icon.png'))
         # self.resize(400, 300)
@@ -25,9 +65,27 @@ class MyApp(QWidget):
         # layout.addWidget(button)
         # layout.addWidget(self.output)
 
-    def say_hello(self):
-        inputText=self.inputField.text()
-        self.output.setText('Hello {}'.format(inputText))
+    def state_machine(self):
+
+        if self.step=="Focus":
+            self.short_label.setStyleSheet("background-color: grey")
+            self.focus_label.setStyleSheet("background-color: blue")
+            worker=Worker(10)
+        elif self.step=="Short":
+            self.short_label.setStyleSheet("background-color: blue")
+            self.focus_label.setStyleSheet("background-color: grey")
+            worker=Worker(2)
+        worker.signals.finished.connect(self.actual_state)
+        self.threadpool.start(worker)
+    def actual_state(self):
+        if self.step == "Focus":
+            self.step="Short"
+            self.short_label.setStyleSheet("background-color: green")
+            self.focus_label.setStyleSheet("background-color: grey")
+        else :
+            self.step="Focus"
+            self.short_label.setStyleSheet("background-color: grey")
+            self.focus_label.setStyleSheet("background-color: green")
 
 # Pass in sys.argv to allow command line arguments
 app = QApplication(sys.argv)
